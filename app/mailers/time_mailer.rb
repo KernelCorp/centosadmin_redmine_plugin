ecb97cdcdef37b378f13d2927e39dbde29738294
Dmitry Maganov <vonagam@gmail.com>
2014-07-07 19:11:20 +0700

    change email body

--- app/mailers/time_mailer.rb
+++ app/mailers/time_mailer.rb
@@ -1,6 +1,10 @@
 # encoding: UTF-8
 module CentosAdmin
   class TimeMailer < ActionMailer::Base
+    def self.default_url_options
+      { host: Setting.host_name, protocol: Setting.protocol }
+    end
+
     def little_time( project )
       @project = project
       emails = @project.managers.map{ |m| m.user.mail }
